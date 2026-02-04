9fd0ea4e611d5034770b513f89e6818908189bc6
Jorge Armando Cuaxospa Jiménez <armjorge@gmail.com>
2026-02-04 03:59:01 -0600

    Fix API endpoint for fruit requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         #st.text(smoothiefroot_response.json())
         st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
     
